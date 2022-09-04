6c52baca0e65103c848c5cc9b184ed212675b7a7
Alex <magefast@gmail.com>
2022-09-04 15:24:25 +0300

    Magento 2 Module

--- registration.php
+++ registration.php
@@ -8,3 +8,4 @@ declare(strict_types=1);
 use Magento\Framework\Component\ComponentRegistrar;
 
 ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Magefast_SitemapNoOutofstockProducts', __DIR__);
+
